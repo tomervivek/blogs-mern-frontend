e75dc7316146864d5e5edd2269d6c2e99b9fbaef
vivek singh <vivek@gmail.com>
2025-06-22 16:47:29 +0530

    PWA

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { SavedBlogProvider } from './SavedBlogContext';
 import App from './App';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 ReactDOM.render(
   <SavedBlogProvider>
     <App />
   </SavedBlogProvider>,
   document.getElementById('root')
 );
+serviceWorkerRegistration.register();
